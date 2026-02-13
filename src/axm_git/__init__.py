99f2911e6b656ecaeb20d69af8dc995afcd48d31
JarryGabriel <jarry.gabriel@gmail.com>
2026-02-13 21:03:18 +0100

    feat: initial scaffold

---
+++ src/axm_git/__init__.py
@@ -0,0 +1,10 @@
+"""axm-git - A modern Python package"""
+
+from axm_git._version import __version__
+
+__all__ = ["__version__"]
+
+
+def hello() -> str:
+    """Return a greeting message."""
+    return "Hello from axm-git!"
